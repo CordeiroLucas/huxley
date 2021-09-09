0e757d9764aca55e9fa8da515dba757c5b5cc05e
Lucas Cordeiro Pacheco <pacheco.lucascordeiro@gmail.com>
2021-09-09 14:50:24 -0300

    Nova Questão
    Questão de estrutura de dados, fazer uma lista de compras para, adicionar, remover, organizar itens e no final ter o preço.

---
+++ nivel3/listaCompras.c
@@ -0,0 +1,29 @@
+/*
+
+QUESTÃO Lista de Compras
+
+Os monitores de Introdução a Programação tentando fazer a comemoração de fim de ano, começaram a fazer a
+ lista de itens que eles comprariam pra a festa pra ter uma ideia de quanto iriam gastar.
+
+O problema é que como o grupo era muito grande, ficava muito difícil de operar a lista porque várias pessoas 
+ estavam constantemente inserindo novos itens e removendo, então eles decidiram pedir sua ajuda para que 
+ conseguissem computar direitinho o preço total da festa.
+
+Você precisa criar um programa que aceite os seguintes comandos:
+
+INSERIR NOME VALOR QUANTIDADE - insere um novo produto com o Nome, Valor de compra e Quantidade de itens
+REMOVER NOME QUANTIDADE - remove uma determinada quantidade de itens (ou todos) de um produto pelo Nome
+REMOVERGRUPO VALOR -  remove todos os produtos que tem seu valor total (por item) maiior que VALOR
+CONSULTAR - consulta o preço total da Lista até o momento
+PROCURAR NOME - procura se o produto de Nome está na lista e retorna seu Nome, Valor e Quantidade de itens
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+	
+	return 0;
+}
